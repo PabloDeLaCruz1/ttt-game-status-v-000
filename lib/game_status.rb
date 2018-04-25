b2934ef0fc35b1bd70d2d61e9b697a66e354b240
Pablo De La Cruz <pdelac@ramapo.edu>
2018-04-25 22:29:39 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,7 +25,6 @@ def won?(board)
     position_2 = board[win_index_2] # load the value of the board at win_index_2
     position_3 = board[win_index_3] # load the value of the board at win_index_3
     
-    puts win_combination
      if position_1 == "X" && position_2 == "X" && position_3 == "X"
        win_combination 
      else
